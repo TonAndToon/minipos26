76553b820dc75e088e1102584eb6c0c81c815b22
Namopodtaya <Namo@abc.com>
2026-04-06 10:09:29 +0700

    Start build web Mini POS video 1

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Hello Vue Web 7889</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+		name: 'Minipos26App',
+		
+		data() {
+		  return {
+
+          };
+		 },
+		  mounted() {
+
+          },
+		 methods: {},
+		  
+         
+		
+}
+</script>
+
+<style lang= "scss" scoped>
+
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Home Vue Web 12345</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+		name: 'Minipos26App',
+		
+		data() {
+		  return {
+
+          };
+		 },
+		  mounted() {
+
+          },
+		 methods: {},
+		  
+         
+		
+}
+</script>
+
+<style lang= "scss" scoped>
+
+</style>
